295176a84d4ba8d45e417af040d4bdc36e50a83f
Gabriel <Gabriel@desktops.com>
2026-04-25 08:11:18 -0300

    Fix: Remove scratch files causing build errors

--- test_phone.ts
+++
@@ -1,8 +0,0 @@
-import { formatPhone } from './app/utils/formatPhone.ts';
-
-console.log('1 ->', formatPhone('1'));
-console.log('11 ->', formatPhone('11'));
-console.log('119 ->', formatPhone('119'));
-console.log('119999 ->', formatPhone('119999'));
-console.log('1199999 ->', formatPhone('1199999'));
-console.log('11999999999 ->', formatPhone('11999999999'));
